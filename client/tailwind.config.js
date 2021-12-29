df72fa9a692471e55aa6e2978e75edf857252ef4
matton <masato_ikegawa612400@yahoo.co.jp>
2021-12-30 02:18:25 +0900

    purgeすると不要なcssを追い出せる

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   darkMode: false,
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx,html}",
+  ],}
 };
